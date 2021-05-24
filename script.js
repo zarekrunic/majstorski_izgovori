0430de1dac4e0cc5f1c2018edc6b8962003b0d4a
Zare <zarekrunic@icloud.com>
2021-05-24 15:57:39 +0200

    style

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let izgovori = ["Ispraviće se kad se osuši.",
-                    "Bolje malo pivo nego veliko hvala.",
-                    "to se kuća sleže",
-                    "nemoj da se nerviraš, biće ok",
-                    "dolazim krajem nedelje",
-                    "to su ovi prošli zeznuli",
-                    "popravićemo sledeći put",
-                    "ma ne mora da se malteriše, samo lepak i stiropor",
-                    "nije do nas, loše su izlili košuljicu",
-                    "kada se okreči u belo, neće se videti",
-                    "čini ti se, oće oko da vara",
-                    "imam ja čoveka za to, vrhunski majstor"];
+  "Bolje malo pivo nego veliko hvala.",
+  "to se kuća sleže",
+  "nemoj da se nerviraš, biće ok",
+  "dolazim krajem nedelje",
+  "to su ovi prošli zeznuli",
+  "popravićemo sledeći put",
+  "ma ne mora da se malteriše, samo lepak i stiropor",
+  "nije do nas, loše su izlili košuljicu",
+  "kada se okreči u belo, neće se videti",
+  "čini ti se, oće oko da vara",
+  "imam ja čoveka za to, vrhunski majstor"];
 
 function izgovor() {
   let rand = Math.floor(Math.random() * izgovori.length);
-  
+
   let content = izgovori[rand];
 
   document.getElementById('izgovor').innerHTML = content;
