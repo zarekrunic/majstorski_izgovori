64f2bafbd704fa201575f269ca8bf0e963f9fb3a
Zare <zarekrunic@icloud.com>
2021-05-24 15:40:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-var izgovori;
-            izgovori = ["Ispraviće se kad se osuši.", "Bolje malo pivo nego veliko hvala.", "to se kuća sleže", "nemoj da se nerviraš, biće ok", "dolazim krajem nedelje", "to su ovi prošli zeznuli", "popravićemo sledeći put", "ma ne mora da se malteriše, samo lepak i stiropor", "nije do nas, loše su izlili košuljicu", "kada se okreči u belo, neće se videti", "čini ti se, oće oko da vara", "imam ja čoveka za to, vrhunski majstor"];
-                       function randGen() {
-              return Math.floor(Math.random() * 5);
-            }
+let receniceIzgovori;
+receniceIzgovori = ["Ispraviće se kad se osuši.",
+                    "Bolje malo pivo nego veliko hvala.",
+                    "to se kuća sleže",
+                    "nemoj da se nerviraš, biće ok",
+                    "dolazim krajem nedelje",
+                    "to su ovi prošli zeznuli",
+                    "popravićemo sledeći put",
+                    "ma ne mora da se malteriše, samo lepak i stiropor",
+                    "nije do nas, loše su izlili košuljicu",
+                    "kada se okreči u belo, neće se videti",
+                    "čini ti se, oće oko da vara",
+                    "imam ja čoveka za to, vrhunski majstor"];
 
-            function izgovor() {
-              var rand = Math.floor(Math.random() * 10);
-              
-              var content = izgovori[rand];
+function izgovor() {
+  let rand = Math.floor(Math.random() * 10);
+  
+  let content = receniceIzgovori[rand];
 
-              document.getElementById('izgovor').innerHTML = content;
-            };
-            izgovor();
+  document.getElementById('izgovor').innerHTML = content;
+};
+izgovor();
