0501f1530ae31ce5d58d91c123827196e77a0cc1
Zare <zarekrunic@icloud.com>
2021-05-24 15:53:54 +0200

    sitne izmene

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let receniceIzgovori;
-receniceIzgovori = ["Ispraviće se kad se osuši.",
+let izgovori = ["Ispraviće se kad se osuši.",
                     "Bolje malo pivo nego veliko hvala.",
                     "to se kuća sleže",
                     "nemoj da se nerviraš, biće ok",
@@ -13,9 +12,9 @@ receniceIzgovori = ["Ispraviće se kad se osuši.",
                     "imam ja čoveka za to, vrhunski majstor"];
 
 function izgovor() {
-  let rand = Math.floor(Math.random() * 10);
+  let rand = Math.floor(Math.random() * izgovori.length);
   
-  let content = receniceIzgovori[rand];
+  let content = izgovori[rand];
 
   document.getElementById('izgovor').innerHTML = content;
 };
